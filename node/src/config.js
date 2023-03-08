3833dd5272e86ac94d939a4d4819270f8e4ac463
CoconutsXXS <pistoman47@gmail.com>
2023-03-08 17:44:48 +0100

    Commit

---
+++ node/src/config.js
@@ -0,0 +1,14 @@
+import dotenv from '../node_modules/dotenv';
+dotenv.config();
+
+export const firebaseConfig = 
+{
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "quadrinity-2bfc0.firebaseapp.com",
+    databaseURL: "https://quadrinity-2bfc0-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: "quadrinity-2bfc0.appspot.com",
+    messagingSenderId: "64702278796",
+    appId: "1:64702278796:web:324b7518c19d8f2d481fa9",
+    measurementId: "G-TT93HV11JL"
+};
